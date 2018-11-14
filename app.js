90195124ff411ce1b89a47905a0737b60f05284c
Pamela Hutchinson <pamelahutchinson@Pamelas-MBP.localdomain>
2018-11-14 09:41:26 -0600

    changed category value

--- app.js
+++ app.js
@@ -102,18 +102,23 @@ app.get('/index',function(req,res){
 //fetch a particular category
 app.post('/select-category',function(req,res){
     let ddViewBy = req.body.ddViewBy
-    
-    models.transaction.findAll({
-        where:{
-            category: ddViewBy,
-            userid: req.session.userid
-        }
-    }).then(function(category){
-        res.render('index',{category:category})
-        console.log(category.amount)
-    })
+  
+    if (ddViewBy == "All") {
+      res.redirect('index')
+    }else {
+        models.transaction.findAll({
+            where:{
+                category: ddViewBy,
+                userid: req.session.userid
+            }
+        }).then(function(category){
+            res.render('index',{category:category})
+            
+        })
+      }
 
 })  
+
 
 
 
--- models/user.js
+++ models/user.js
@@ -3,8 +3,7 @@ module.exports = (sequelize, DataTypes) => {
   const user = sequelize.define('user', {
     username: DataTypes.STRING,
     password: DataTypes.STRING,
-    email: DataTypes.STRING,
-    monthly_income: DataTypes.DOUBLE
+    email: DataTypes.STRING
   }, {});
   user.associate = function(models) {
     user.hasMany(models.transaction, {as: 'transaction', foreignKey: 'userid'})
